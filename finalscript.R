4abc7b45dec16ca04fc5274636ded0bbaeda02d7
olivrwitt <olivrwitt@gmail.com>
2026-08-24 11:39:58 -0700

    process

---
+++ finalscript.R
@@ -0,0 +1,11 @@
+# # How will you get the data into R?
+# - read_csv
+# # How will you clean up the data?
+# - remove NA values
+# - reshape data if necessary
+# # Are any processing or modeling steps required?
+# - moving average is needed
+# # What are your outputs?
+# - figure
+# - averages for 9-week moving average windows
+# - ggplot
