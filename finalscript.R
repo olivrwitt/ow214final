95802ddee95dab9cfff33d21115ae12f3a48cca9
olivrwitt <olivrwitt@gmail.com>
2026-08-26 15:00:06 -0700

    comment

--- finalscript.R
+++ finalscript.R
@@ -1,11 +1,10 @@
-# # How will you get the data into R?
-# - read_csv
-# # How will you clean up the data?
-# - remove NA values
-# - reshape data if necessary
-# # Are any processing or modeling steps required?
-# - moving average is needed
-# # What are your outputs?
-# - figure
-# - averages for 9-week moving average windows
-# - ggplot
+## read data
+library(tidyverse)
+bisley1 <- read_csv("data/bisley1.csv")
+bisley2 <- read_csv("data/bisley2.csv")
+bisley3 <- read_csv("data/bisley3.csv")
+
+## clean data
+# remove irrelevant columns
+# remove NA values
+
