1f6b3b7d93fda11031ee77711eac4f003a15ec0d
olivrwitt <olivrwitt@gmail.com>
2026-08-27 11:06:00 -0700

    self assessment

--- finalscript.R
+++ finalscript.R
@@ -1,11 +1,52 @@
-# # How will you get the data into R?
-# - read_csv
-# # How will you clean up the data?
-# - remove NA values
-# - reshape data if necessary
-# # Are any processing or modeling steps required?
-# - moving average is needed
-# # What are your outputs?
-# - figure
-# - averages for 9-week moving average windows
-# - ggplot
+library(tidyverse)
+
+
+source("R/moving-average.R")
+
+bisley1 <- read_csv("data/bisley1.csv")
+bisley2 <- read_csv("data/bisley2.csv")
+bisley3 <- read_csv("data/bisley3.csv")
+prm <- read_csv("data/prm.csv")
+
+#select relevant columns and label by site
+cleaned_bis_1 <- bisley1 |> 
+  select(`NH4-N`, `NO3-N`, Sample_Date, K, Ca, Mg) |> 
+  mutate(Site = "BQ1")
+cleaned_bis_2 <- bisley2 |> 
+  select(`NH4-N`, `NO3-N`, Sample_Date, K, Ca, Mg) |> 
+  mutate(Site = "BQ2")
+cleaned_bis_3 <- bisley3 |> 
+  select(`NH4-N`, `NO3-N`, Sample_Date, K, Ca, Mg) |> 
+  mutate(Site = "BQ3")
+cleaned_prm <- prm |> 
+  select(`NH4-N`, `NO3-N`, Sample_Date, K, Ca, Mg) |> 
+  mutate(Site = "PRM")
+
+bq1_window <- moving_average(cleaned_bis_1)
+bq2_window <- moving_average(cleaned_bis_2)
+bq3_window <- moving_average(cleaned_bis_3)
+prmwindow <- moving_average(cleaned_prm)
+
+
+complete <- bind_rows(bq1_window, bq2_window, bq3_window, prmwindow)
+complete_and_longer <- complete |> 
+  pivot_longer(
+    cols = c(`NH4-N`, `NO3-N`, K, Ca, Mg),
+    names_to = "nutrient", 
+    values_to = "concentration"
+    )
+
+
+ggplot(
+  data = complete_and_longer,
+  mapping = aes(
+    x = window_start,
+    y = concentration,
+    linetype = Site
+  )
+) + geom_line()+
+  facet_wrap(
+    vars(nutrient), 
+    scales = "free", 
+    ncol = 1
+  )
---
+++ self-assessment.md
@@ -0,0 +1,15 @@
+### Automate
+Not Yet.
+
+Data reading and cleaning is handled in a standalone script that creates intermediate outputs (spaghetti.R).  However, my Quarto document currently just contains text, rather than completing analysis and reading intermediate outputs.  All scripts run without errors.
+
+### Organize
+Meets Spec
+
+Data and code are organized in labeled separate folders.  Raw data is contained in the data folder, function R scripts are in the R folder, and "spaghetti" code is contained in the scratch folder.  The "moving average" function is defined in "R/", and then used elsewhere.  All code outside the scratch folder is used.
+
+### Document
+Meets Spec
+
+The repository contains an effective README with a short but descriptive title and a brief explanation of the repository’s purpose
+A concise description of what’s housed in the repository, details regarding data access, and references are all included.
