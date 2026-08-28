f3357ce7c5104d1550ae570c9ece7e385863eff5
olivrwitt <olivrwitt@gmail.com>
2026-08-28 10:13:33 -0700

    added code comments

--- 1_clean_data.R
+++ 1_clean_data.R
@@ -7,6 +7,7 @@ bisley2 <- read_csv("data/bisley2.csv")
 bisley3 <- read_csv("data/bisley3.csv")
 prm <- read_csv("data/prm.csv")
 
+#clean datasets (select relevant columns)
 cleaned_bis_1 <- bisley1 |>
   select(`NH4-N`, `NO3-N`, Sample_Date, K, Ca, Mg) |>
   mutate(Site = "BQ1")
@@ -20,11 +21,13 @@ cleaned_prm <- prm |>
   select(`NH4-N`, `NO3-N`, Sample_Date, K, Ca, Mg) |>
   mutate(Site = "PRM")
 
+#call moving average function to take window averages
 bq1_window <- moving_average(cleaned_bis_1)
 bq2_window <- moving_average(cleaned_bis_2)
 bq3_window <- moving_average(cleaned_bis_3)
 prmwindow <- moving_average(cleaned_prm)
 
+#combine datasets and pivot
 complete <- bind_rows(bq1_window, bq2_window, bq3_window, prmwindow)
 complete_and_longer <- complete |>
   pivot_longer(
@@ -33,4 +36,5 @@ complete_and_longer <- complete |>
     values_to = "concentration"
   )
 
+# write output to output folder
 write_csv(complete_and_longer, "output/clean_combine_pivot.csv")
--- R/moving-average.R
+++ R/moving-average.R
@@ -1,5 +1,6 @@
 moving_average <- function(cleaned_data) {
   result <- tibble(
+    #set up 9 week windows and empty data frame
     window_start = seq(ymd("1986-05-20"), ymd("1994-12-30"), by = "9 weeks"),
     Site = NA,
     `NH4-N` = NA,
@@ -9,14 +10,17 @@ moving_average <- function(cleaned_data) {
     Mg = NA
   )
   for (i in 1:nrow(result)) {
+    #iterate through cleaned data
     w1 <- result$window_start[i]
     w2 <- w1 + weeks(9)
     in_window <- cleaned_data$Sample_Date >= w1 & cleaned_data$Sample_Date < w2
+    # take all values that fall in the window
     k_window <- cleaned_data$K[in_window]
     Ca_window <- cleaned_data$Ca[in_window]
     Mg_window <- cleaned_data$Mg[in_window]
     NH4N_window <- cleaned_data$`NH4-N`[in_window]
     NO3N_window <- cleaned_data$`NO3-N`[in_window]
+    #means of values in window
     result$K[i] <- mean(k_window, na.rm = TRUE)
     result$Ca[i] <- mean(Ca_window, na.rm = TRUE)
     result$Mg[i] <- mean(Mg_window, na.rm = TRUE)
